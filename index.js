48074824d3bccf13299dd2d1031844225f7825dd
Lucas <LucasTroncoso176@gmail.com>
2025-01-10 15:33:57 -0300

    controladores de ruta

--- index.js
+++ index.js
@@ -35,36 +35,27 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const { id } = request.params
-    const note = notes.find(note => note.id === Number(id))
-    if(note){
+    Note.findById(request.params.id).then( note => {
         response.json(note)
-    }else{
-        response.status(404).end()
-    }
+    })
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(note => note.id))
-    : 0
-    return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
+
     if(!body.content){
         return response.status(400).json({
             error: "content missing"
         })
     }
-    const note = {
+    const note = new Note({
         content: body.content,
         important: Boolean(body.important) || false,
-        id: generateId()
-    }
-    notes = notes.concat(note)
-    response.json(note)
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
